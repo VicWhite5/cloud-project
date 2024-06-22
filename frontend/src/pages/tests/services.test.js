3987dc837cdec5fe2b6e1e4d738f3b1e705ed4f1
Oriana Toubia <ovtoubia.20@est.ucab.edu.ve>
2024-06-22 14:55:59 -0400

    Services Tests
    Test de renderizado de pagina de servicios (menu)

---
+++ frontend/src/pages/tests/services.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Services from '../Services/Services';
+import '@testing-library/jest-dom';
+
+describe('Services', () => {
+  it('renders without crashing', () => {
+    render(<Router>
+        <Services />
+      </Router>);
+    expect(screen.getByTestId('services')).toBeInTheDocument();
+  });
+
+  it('renders the Sidebar component', () => {
+    render(<Router>
+        <Services />
+      </Router>);
+    expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+  });
+  it('renders the Navbar component', () => {
+    render(<Router>
+        <Services />
+      </Router>);
+    expect(screen.getByTestId('navbar')).toBeInTheDocument();
+  });
+  it('renders the DatatableServices component', () => {
+    render(<Router>
+        <Services />
+      </Router>);
+    expect(screen.getByTestId('datatableServices')).toBeInTheDocument();
+  });
+});
