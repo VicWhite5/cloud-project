a81af80ac4ca0c1844d3653e5c49ef7b92ab7ee5
victor <vmblanco.20@est.ucab.edu.ve>
2024-06-24 20:19:41 +0000

    Se agregaron archivos de configuracion para kubernetes v6

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -18,7 +18,7 @@ import { AuditModule } from './audit/audit.module';
     // ),
 
     MongooseModule.forRoot(
-      `mongodb://root:$password@mongo-service/ramen-db?`
+      `mongodb://root:password@mongo-service/ramen-db?`
     ),
 
     RamenModule,
